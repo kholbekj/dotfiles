f85e51eee4aa23fac6119e19ffed7399e9e3dfb5
Kasper Holbek Jensen <kholbekj@gmail.com>
2017-05-15 00:17:12 +0200

    bring back spotify, and add pairing tools

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -51,7 +51,7 @@ set-option -g display-panes-colour $tm_color_inactive
 set-window-option -g clock-mode-colour $tm_color_active
 
 # tm_tunes="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/tunes.scpt | cut -c 1-50)"
-tm_tunes="#[fg=$tm_color_music]#(osascript -l JavaScript ~/.dotfiles/applescripts/tunes.js)"
+tm_tunes="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/tunes.scpt)"
 tm_battery="#(~/.dotfiles/bin/battery_indicator.sh)"
 
 tm_date="#[fg=$tm_color_inactive] %R %d %b"
